d6addf127c9f972902ce7b86d485f6ab2b42a425
박지윤 <jyun1937@naver.com>
2023-05-12 17:43:51 +0900

    path

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src/', import.meta.url)),
     },
   },
-  base: 'https://jiyun-parkk.github.io/vue-todolist/dist/',
+  base: '/vue-todolist/',
 });
