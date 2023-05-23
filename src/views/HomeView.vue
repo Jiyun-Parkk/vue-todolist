f141faa8851a1551ef3842f648a558e06660c99a
박지윤 <jyun1937@naver.com>
2023-05-23 21:26:02 +0900

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,17 @@
 <script setup>
-import { ref } from 'vue'
-import AppHeader from '@/components/AppHeader.vue'
-import AppSection from '@/components/AppSection.vue'
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
-import { useTodoStore } from '@/stores/useTodoStore.js'
-const store = useTodoStore()
-const list = JSON.parse(localStorage.getItem('todos'))
-list && store.actions.setTodo(list)
+import { ref } from 'vue';
+import AppHeader from '@/components/AppHeader.vue';
+import AppSection from '@/components/AppSection.vue';
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+import { useTodoStore } from '@/stores/useTodoStore.js';
+const store = useTodoStore();
+const list = JSON.parse(localStorage.getItem('todos'));
+list && store.actions.setTodo(list);
 </script>
 
 <template>
   <DefaultLayout>
+    <h2>todo</h2>
     <AppHeader />
     <AppSection />
   </DefaultLayout>
